ac04940d060946e71f9a67ac9b7e940069cd89a9
alfayohannis <alfa.yohannis@york.ac.uk>
2021-06-04 10:08:26 +0100

    Some updates

---
+++ interledger/transfer_money.sh
@@ -0,0 +1,52 @@
+alias   alice-cli="sudo docker run --rm --network local-ilp interledgerrs/ilp-cli --node http://alice-node:7770"
+alias     bob-cli="sudo docker run --rm --network local-ilp interledgerrs/ilp-cli --node http://bob-node:7770"
+alias charlie-cli="sudo docker run --rm --network local-ilp interledgerrs/ilp-cli --node http://charlie-node:7770"
+
+printf "\n========= ALICE'S NODE ========="
+printf "\nAlice's balance: "
+alice-cli accounts balance alice --auth hi_alice
+printf "Bob's balance: "
+alice-cli accounts balance bob --auth hi_alice
+
+printf "\n========= BOB'S NODE ========="
+printf "\nAlice's balance: "
+bob-cli accounts balance alice --auth hi_bob
+printf "Charlie's balance: "
+bob-cli accounts balance charlie --auth hi_bob
+
+printf "\n========= CHARLIE'S NODE ========="
+printf "\nBob's balance: "
+charlie-cli accounts balance bob --auth hi_charlie
+printf "Charlie's balance: "
+charlie-cli accounts balance charlie --auth hi_charlie
+
+alice-cli pay alice \
+  --auth alice_password \
+  --amount 200000 \
+  --to http://charlie-node:7770/accounts/charlie/spsp
+
+# sudo docker run --rm --network local-ilp interledgerrs/ilp-cli \
+#   --node http://alice-node:7770 pay alice \
+#   --auth alice_password \
+#   --amount 200000 \
+#   --to http://charlie-node:7770/accounts/charlie/spsp
+
+printf "\n========= ALICE'S NODE ========="
+printf "\nAlice's balance: "
+alice-cli accounts balance alice --auth hi_alice
+printf "Bob's balance: "
+alice-cli accounts balance bob --auth hi_alice
+
+printf "\n========= BOB'S NODE ========="
+printf "\nAlice's balance: "
+bob-cli accounts balance alice --auth hi_bob
+printf "Charlie's balance: "
+bob-cli accounts balance charlie --auth hi_bob
+
+printf "\n========= CHARLIE'S NODE ========="
+printf "\nBob's balance: "
+charlie-cli accounts balance bob --auth hi_charlie
+printf "Charlie's balance: "
+charlie-cli accounts balance charlie --auth hi_charlie
+
+printf "\n==========================================================\n"
